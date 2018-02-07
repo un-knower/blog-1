da2bc12545177643b474ed2e1b057c6fd822ea79
kaidata <kaidataanalysis@gmail.com>
2018-02-07 18:05:28 +0800

    2018-2-7 18:05:25

---
+++ src/py/learnTF/com/chaosdata/deeplearning/learn/sklearn/naivebayes/naive_bayes_demo.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+import numpy as np
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+
+from sklearn.naive_bayes import GaussianNB
+
+clf = GaussianNB()
+clf.fit(X, Y)
+print(clf.predict([[-0.8, -1]]))
+
+clf_df = GaussianNB
+clf_df.partial_fit(X, Y, np.unique(Y))
+print(clf_df.predict([[-0.8, -1]]))
