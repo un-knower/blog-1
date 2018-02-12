cf0ce033956b3719b01e74d10175fca403bc9159
kaidata <kaidataanalysis@gmail.com>
2018-02-12 12:38:30 +0800

    2018-2-12 12:38:23

--- src/py/learnTF/com/chaosdata/deeplearning/learn/tutorial/brfss_learn.py
+++ src/py/learnTF/com/chaosdata/deeplearning/learn/tutorial/brfss_learn.py
@@ -1,62 +1,99 @@
 # coding=utf-8
-import feather
 import pandavro as pdx
 
 import project_infos
 
-import numpy as np
+import pandas as pd
+
+import feather
+import pandas
+import glob
+
 
 # import os
 
 # print(os.path.abspath('brfss.py'))
 # pip3 install -U pandas
 
-# DATASET:https://www.cdc.gov/brfss/annual_data/2015/files/LLCP2015ASC.zip
+def io():
+    # DATASET:https://www.cdc.gov/brfss/annual_data/2015/files/LLCP2015ASC.zip
 
-# df = brfss.ReadBrfss(filename=project_infos.project_dir + '/data/dataset/LLCP2015ASC.zip')
+    # df = brfss.ReadBrfss(filename=project_infos.project_dir + '/data/dataset/LLCP2015ASC.zip')
 
-# df = pandas.read_pickle(path=project_infos.project_dir + '/data/dataset/LLCP2015.pickle')
+    # df = pandas.read_pickle(path=project_infos.project_dir + '/data/dataset/LLCP2015.pickle')
 
-"""
-pip3 install -U feather-format
-"""
-# df = feather.read_dataframe(source=project_infos.project_dir + '/data/dataset/LLCP2015.feather')
+    """
+    pip3 install -U feather-format
+    """
+    # df = feather.read_dataframe(source=project_infos.project_dir + '/data/dataset/LLCP2015.feather')
 
-df = pdx.from_avro(project_infos.project_dir + '/data/dataset/LLCP2015.avro')
+    # df = pdx.from_avro(project_infos.project_dir + '/data/dataset/LLCP2015.avro')
 
-# df.to_pickle(path=project_infos.project_dir + '/data/dataset/LLCP2015.pickle')
 
-# feather.write_dataframe(df=df, dest=project_infos.project_dir + '/data/dataset/LLCP2015.feather')
+    df = pd.read_parquet(path=project_infos.project_dir + '/data/dataset/LLCP2015.parquet')
 
-"""
-ImportError: No module named 'openpyxl'
-pip3 install -U openpyxl
-"""
-# df.to_excel(project_infos.project_dir + '/data/dataset/LLCP2015.xlsx', index=False)
+    # df.to_pickle(path=project_infos.project_dir + '/data/dataset/LLCP2015.pickle')
 
-"""
-pip3 install -U pandavro
-"""
-#pdx.to_avro(project_infos.project_dir + '/data/dataset/LLCP2015.avro', df)
+    feather.write_dataframe(df=df, dest=project_infos.project_dir + '/data/dataset/LLCP2015.feather')
 
-print(df.ftypes)
+    """
+    ImportError: No module named 'openpyxl'
+    pip3 install -U openpyxl
+    """
+    # df.to_excel(project_infos.project_dir + '/data/dataset/LLCP2015.xlsx', index=False)
 
-print(df.dtypes)
+    """
+    pip3 install -U pandavro
+    """
+    # pdx.to_avro(project_infos.project_dir + '/data/dataset/LLCP2015.avro', df)
 
-print(df.head(10))
+    # df.to_parquet(fname=project_infos.project_dir + '/data/dataset/LLCP2015.parquet')
 
-# df_next = df.astype({'sex': np.bool, 'exercise': np.bool, 'fruit': np.bool, 'vegetable': np.bool})
+    print(df.ftypes)
 
-# pdx.to_avro(project_infos.project_dir + '/data/dataset/LLCP2015.avro', df_next)
+    print(df.dtypes)
 
-# print(df_next.ftypes)
-#
-# print(df_next.dtypes)
-#
-# print(df_next.head())
-#
-# df_part = df_next[['sex', 'age', 'income', 'bmi', 'height', 'weight']]
+    print(df.head(10))
 
-# df_head = df_part.head()
+    print()
 
-# print(df_head)
+    print(df.count())
+
+    print()
+
+    print(df.describe())
+
+    # df_next = df.astype({'sex': np.bool, 'exercise': np.bool, 'fruit': np.bool, 'vegetable': np.bool})
+
+    # pdx.to_avro(project_infos.project_dir + '/data/dataset/LLCP2015.avro', df_next)
+
+    # print(df_next.ftypes)
+    #
+    # print(df_next.dtypes)
+    #
+    # print(df_next.head())
+    #
+    # df_part = df_next[['sex', 'age', 'income', 'bmi', 'height', 'weight']]
+
+    # df_head = df_part.head()
+
+    # print(df_head)
+
+
+def csv2feather(dir_path):
+    # http://files.grouplens.org/datasets/movielens/ml-latest.zip
+    for path in glob.glob(dir_path + "/*.csv"):
+        print(path)
+
+        df = pandas.read_csv(path, engine='python', sep=None)
+
+        print(df.head(10))
+
+        print(df.ftypes)
+
+        feather.write_dataframe(df, path.replace('.csv', '.feather'))
+        df.to_parquet(fname=path.replace('.csv', '.parquet'))
+
+
+dir_path = "C:\\Users\\likai14\\Downloads\\ml-latest"
+csv2feather(dir_path)
